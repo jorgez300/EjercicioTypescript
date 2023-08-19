8c210f7edd2b6d0a6dfde6aacad26b70b8d39a47
jorgez300 <ing.jorge.gonzalez92@gmail.com>
2023-08-19 15:54:57 -0400

    Capitulo 1
    https://www.youtube.com/watch?v=gieEQFIfgYc&ab_channel=DaveGray

---
+++ TypeScript Bases/Introduccion/Fundamentos/build/js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let user2 = 'x';
+console.log(user2);
+let a = 3;
+let b = 3;
+console.log(a * b);
---
+++ TypeScript Bases/Introduccion/Fundamentos/src/main.ts
@@ -0,0 +1,12 @@
+let user2 = 'x';
+console.log(user2);
+
+let a: number = 3;
+let b: number = 3;
+
+console.log(a * b);
+
+
+
+
+
